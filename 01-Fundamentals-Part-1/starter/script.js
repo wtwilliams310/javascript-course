a78d9ae867ea31b7a7b814b61d854c2ab6364171
Titus Williams <wtwilliams310@gmail.com>
2023-01-14 17:15:30 -0600

    des:completed Values and Variables exercises

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,3 +3,13 @@ console.log(40 + 20 * 2 / 3)
 
 let firstName = 'Jonas'
 
+console.log(firstName);
+
+console.log(js);
+
+
+let country = 'United States'
+let continent = 'North America'
+let population = '332 million'
+
+console.log(country, continent);
