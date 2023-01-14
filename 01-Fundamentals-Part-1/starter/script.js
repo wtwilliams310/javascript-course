b72acfd7599d45206ac53f731b9252409c8ada2d
Titus Williams <wtwilliams310@gmail.com>
2023-01-14 17:46:15 -0600

    des:completed Data Types exercises

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,15 +1,31 @@
+
 let js = "amazing"
 console.log(40 + 20 * 2 / 3)
 
 let firstName = 'Jonas'
 
-console.log(firstName);
+// console.log(firstName);
 
-console.log(js);
+// console.log(js);
 
 
 let country = 'United States'
 let continent = 'North America'
-let population = '332 million'
+let population = 332000000
 
-console.log(country, continent);
+// console.log(country, continent);
+
+let javascriptIsFun = true
+// console.log(typeof javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
