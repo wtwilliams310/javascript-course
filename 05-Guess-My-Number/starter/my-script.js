e952d958dc613e9b2a0cc8df37f63850cb819d77
Titus Williams <wtwilliams310@gmail.com>
2023-01-27 17:57:31 -0600

    des: completed Section#7 Coding Challenge#1

--- 05-Guess-My-Number/starter/my-script.js
+++ 05-Guess-My-Number/starter/my-script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 let number = document.querySelector('.number');
-let score = document.querySelector('.score');
-let highScore = document.querySelector('.highscore');
+let currentScore = document.querySelector('.current-score');
+let highScore = document.querySelector('#high-score');
 let message = document.querySelector('.message');
 let secretNumber = Math.floor(Math.random() * (21 - 1) + 1);
 
@@ -11,34 +11,35 @@ let scoreRecord = 0;
 
 console.log(secretNumber, 'first secret number');
 
-document.querySelector('.check').addEventListener('click', function () {
-  let guess = Number(document.querySelector('.guess').value);
+document.querySelector('.check-btn').addEventListener('click', function () {
+  let guess = Number(document.querySelector('.number-input').value);
   if (!guess) {
     message.textContent = '⛔️ No number!';
   } else if (guess < secretNumber) {
     message.textContent = 'Guess was too low!';
     count--;
-    score.textContent = count;
+    currentScore.textContent = count;
   } else if (guess > secretNumber) {
-    message.textContent = ' Guess was too high!';
+    message.textContent = 'Guess was too high!';
     count--;
-    score.textContent = count;
+    currentScore.textContent = count;
   } else {
     message.textContent = '🥳 You Got It!!!';
     count += 5;
     number.textContent = guess;
-    score.textContent = count;
+    currentScore.textContent = count;
     if (count > scoreRecord) {
       scoreRecord = count;
       highScore.textContent = count;
-      message.textContent = '🏅🏅You set a new high Score!!!!!!!';
+      message.textContent = '🏅🏅You set a new high Score!';
     }
   }
 });
 
-document.querySelector('.again').addEventListener('click', function () {
+document.querySelector('.replay-btn').addEventListener('click', function () {
   count = 20;
-  score.textContent = 20;
+  currentScore.textContent = 20;
+  document.querySelector('.number-input').value = '';
   message.textContent = 'Start guessing...';
   number.textContent = '?';
   secretNumber = Math.floor(Math.random() * (21 - 1) + 1);
