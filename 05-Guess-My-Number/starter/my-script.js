986e8e1a9e405148a0210da80b453accc59ee9f6
Titus Williams <wtwilliams310@gmail.com>
2023-01-27 22:53:05 -0600

    des: refactored code JS code

--- 05-Guess-My-Number/starter/my-script.js
+++ 05-Guess-My-Number/starter/my-script.js
@@ -6,34 +6,64 @@ let highScore = document.querySelector('#high-score');
 let message = document.querySelector('.message');
 let pageWrapper = document.querySelector('.page-wrapper');
 let secretNumber = Math.floor(Math.random() * (21 - 1) + 1);
+// random number using Math.trunc
+// let secretNumber = Math.truc(Math.random() * 20) + 1;
+
+// When player loses game
+const endGame = () => {
+  message.textContent = '🥴 You lost the game!';
+  count = 0;
+  currentScore.textContent = count;
+  pageWrapper.style.backgroundColor = 'red';
+};
+
+// Displays messages after input
+const messageHTML = html => (message.textContent = html);
+
+// Set current score
+const currentScoreHTML = () => (currentScore.textContent = count);
+
+// Sets background color
+const backgroundColor = color => (pageWrapper.style.backgroundColor = color);
 
 let count = 20;
 let scoreRecord = 0;
 
 console.log(secretNumber, 'first secret number');
 
+// Player clicks 'check' button
 document.querySelector('.check-btn').addEventListener('click', function () {
   let guess = Number(document.querySelector('.number-input').value);
-  if (!guess) {
-    message.textContent = '⛔️ No number!';
-  } else if (guess < secretNumber) {
-    message.textContent = '⬇️ Guess was too low!';
-    count--;
-    currentScore.textContent = count;
-  } else if (guess > secretNumber) {
-    message.textContent = '⬆️ Guess was too high!';
-    count--;
-    currentScore.textContent = count;
+
+  // No input
+  if (!guess || guess < 1) {
+    messageHTML('⛔️ Choose a number between 1 and 20!');
+
+    // Guess is too high or too low
+  } else if (guess !== secretNumber) {
+    if (count > 1) {
+      message.textContent =
+        guess > secretNumber
+          ? '⬆️ Guess was too high!'
+          : '⬇️ Guess was too low!';
+      count--;
+      currentScoreHTML();
+    } else {
+      endGame();
+    }
+
+    // Player guesses correct number
   } else {
-    message.textContent = '🥳 You Got It!!!';
+    messageHTML('🥳 You Got It!!! +5pt Bonus!');
     count += 5;
     number.textContent = guess;
-    currentScore.textContent = count;
+    currentScoreHTML();
     if (count > scoreRecord) {
       scoreRecord = count;
       highScore.textContent = count;
-      pageWrapper.style.backgroundColor = 'rgb(52, 172, 52)';
-      message.textContent = '🏅🏅You set a new high Score!';
+      backgroundColor('rgb(52, 172, 52)');
+      number.style.width = '11rem';
+      messageHTML('🏅🏅You set a new high Score!');
     }
   }
 });
@@ -41,10 +71,10 @@ document.querySelector('.check-btn').addEventListener('click', function () {
 // Resets all values except for highscore
 document.querySelector('.replay-btn').addEventListener('click', function () {
   count = 20;
-  currentScore.textContent = 20;
-  pageWrapper.style.backgroundColor = 'black';
+  currentScoreHTML();
+  backgroundColor('black');
   document.querySelector('.number-input').value = '';
-  message.textContent = 'Start guessing...';
+  messageHTML('Start guessing...');
   number.textContent = '?';
   secretNumber = Math.floor(Math.random() * (21 - 1) + 1);
   console.log(secretNumber, 'new secret number');
