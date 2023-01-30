71a7365249a66adf1b7d94f2a76b21b70a59ae9a
Titus Williams <wtwilliams310@gmail.com>
2023-01-29 21:28:21 -0600

    des: practice js

---
+++ 08-Behind-the-Scenes/starter/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function caldAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+caldAge(1991);
